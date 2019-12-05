1f6666fc3c96ed7309948a71d754c14593e95845
chris <275926510@qq.com>
2019-12-05 19:40:14 +0800

    twoSum.go modify

--- easy/twoSum.go
+++ easy/twoSum.go
@@ -1,32 +1,20 @@
-package main
-
-import "fmt"
-
-/**
- * https://leetcode-cn.com/problems/two-sum/
- */
-func main() {
-	ret := twoSum([]int{2,3,3}, 6)
-	fmt.Printf("ret %v", ret)
-}
-
 func twoSum(nums []int, target int) []int {
-	len := len(nums)
-	ret := make([]int, 2)
+    len := len(nums)
+    ret := make([]int, 2)
 
-	for i := 0; i < len; i++ {
-		sub := target - nums[i]
+    for i := 0; i < len; i++ {
+        sub := target - nums[i]
+        
+        for j := i + 1; j < len; j++ {
+            if nums[j] == sub {
+                ret[0] = i
+                ret[1] = j
+                return ret
+            }
+        }
+    }
 
-		for j := i + 1; j < len; j++ {
-			if nums[j] == sub {
-				ret[0] = i
-				ret[1] = j
-				return ret
-			}
-		}
-	}
-
-	return ret
+    return ret
 }
 
 // func twoSum(nums []int, target int) []int {
@@ -35,9 +23,15 @@ func twoSum(nums []int, target int) []int {
 //     ret := make([]int, 2)
 
 //     for i := 0; i < len; i++ {
-//         v,ok := subMap[nums[i]]
+//         v,ok := subMap[nums[i]] 
 
 //         if ok {
 //             ret[0] = v
 //             ret[1] = i
-
+//             return ret
+//         } else {
+//             subMap[target - nums[i]] = i
+//         }
+//     }
+//     return ret
+// }
